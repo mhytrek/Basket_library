3bd5a17d4715092a5c7cc1058a235dd89031407a
hytrek <hytrek@student.agh.edu.pl>
2024-04-07 11:52:38 +0200

    Skonczone testy i poprawa błędu w szukaniu max deliveries

--- Basket_library/src/main/java/com/ocado/basket/BasketSplitter.java
+++ Basket_library/src/main/java/com/ocado/basket/BasketSplitter.java
@@ -1,17 +1,17 @@
 package com.ocado.basket;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 class ProductException extends RuntimeException{
     public ProductException(String reason) {
         super("Cannot find " + reason);
     }
 }
-
 public class BasketSplitter {
     private Map<String, List<String>> productsDeliveryTypes = new HashMap<>();
     private HashMap<String, Integer> deliveryCounter;
@@ -27,8 +27,20 @@ public class BasketSplitter {
         }
     }
 
+    // getters made for testing only
+    public Map<String, List<String>> getProductsDeliveryTypes() {
+        return productsDeliveryTypes;
+    }
 
-// Counts the max amount of products from a basket that can be delivered by each type
+    public HashMap<String, Integer> getDeliveryCounter() {
+        return deliveryCounter;
+    }
+
+    public Map<String, List<String>> getSplittedDeliveries() {
+        return splittedDeliveries;
+    }
+
+    // Counts the max amount of products from a basket that can be delivered by each type
     public void countDeliveries(List<String> products) throws ProductException {
         deliveryCounter = new HashMap<>();
         for(String product: products) {
@@ -56,7 +68,14 @@ public class BasketSplitter {
     public String getBestDelivery(String product){
         List<String> deliveryTypes = productsDeliveryTypes.get(product);
         deliveryTypes = productsDeliveryTypes.get(product);
-        return Collections.max(deliveryTypes,(delivery1, delivery2) -> Integer.compare(getMaxDeliveries(delivery1), getMaxDeliveries(delivery2)));
+        return Collections.max(deliveryTypes, (delivery1, delivery2) -> {
+            int max1 = getMaxDeliveries(delivery1);
+            int max2 = getMaxDeliveries(delivery2);
+            if (max1 == max2) {
+                return delivery1.compareTo(delivery2);
+            }
+            return Integer.compare(max1, max2);
+        });
     }
 
 
@@ -71,7 +90,7 @@ public class BasketSplitter {
 
 
     // Split the products in basket into delivery groups
-    public Map<String, List <String>> split(List<String> items) {
+    public Map<String, List <String>> split(List<String> items){
         countDeliveries(items);
         splittedDeliveries = new HashMap<>();
         for(String product: items){
--- Basket_library/src/test/java/com/ocado/basket/BasketSplitterTest.java
+++ Basket_library/src/test/java/com/ocado/basket/BasketSplitterTest.java
@@ -2,14 +2,29 @@ package com.ocado.basket;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BasketSplitterTest {
+
+    @Test
+    public void testBasketSplitter(){
+        //when
+        BasketSplitter basketSplitter = new BasketSplitter("src/test/configuration/config.json");
+        Map<String, List<String>> result = basketSplitter.getProductsDeliveryTypes();
+
+        //then
+        Map<String, List<String>> expectedResult = new HashMap<String, List<String>>();
+        expectedResult.put("Carrots (1kg)", Arrays.asList( "Click&Collect", "Express Delivery"));
+        expectedResult.put("Cold Beer (330ml)", Arrays.asList("Express Delivery"));
+        expectedResult.put("Steak (300g)", Arrays.asList("Express Delivery", "Click&Collect"));
+        expectedResult.put("AA Battery (4 Pcs.)", Arrays.asList("Express Delivery", "Courier"));
+        expectedResult.put("Espresso Machine", Arrays.asList("Courier", "Click&Collect"));
+        expectedResult.put("Garden Chair", Arrays.asList("Courier"));
+
+        assertEquals(expectedResult, result);
+    }
 
     @Test
     public void testSplit(){
@@ -37,6 +52,17 @@ public class BasketSplitterTest {
 
         //then
         assertThrows(ProductException.class, () -> basketSplitter.split(items2));
+
+        //Test3
+        //given
+        List<String> items3 = new ArrayList<>(List.of("Steak (300g)", "Carrots (1kg)"));
+
+        //when
+        Map<String, List<String>> result3 = basketSplitter.split(items3);
+
+        //then
+        assertEquals(1, result3.keySet().size());
+
     }
 
 }
